93da22e39cafde67e44eb874631c8f6c26168285
Mr.Li <1752146907@qq.com>
2020-03-18 15:27:39 +0800

    提交application.js

---
+++ src/common/application.js
@@ -0,0 +1,8 @@
+export default {
+	host: 'http://test_api.bqn.life',
+	codeHost: 'http://localhost:8088',
+	imageHost: 'http://localhost:8088',
+	appId: '1003604205986484225',
+	version: '1.0.0',
+	platform: 'Merchant-PC'
+};
