b45286b003a25c3fdcd13e03b7721362f2334705
1752146907@qq.com <1752146907@qq.com>
2020-03-27 16:56:20 +0800

    api测试

--- src/common/application.js
+++ src/common/application.js
@@ -1,4 +1,7 @@
 export default {
+	// host: 'http://192.168.33.249:81',
+	// imageHost: 'http://192.168.33.249:81/adminFIle/'
+
 	host: 'http://api.sticweb.com',
 	imageHost: 'http://api.sticweb.com/adminFile/'
 };
